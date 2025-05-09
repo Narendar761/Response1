cfa940540a9db8d4885a0d7a392f6f44cfe4b346
Narendar761 <145740954+Narendar761@users.noreply.github.com>
2025-05-09 12:17:12 +0530

    Create Response1.js

---
+++ Response1.js
@@ -0,0 +1,28 @@
+{
+  "url": "https://www.facebook.com/share/v/1ADAV3rUNL/",
+  "id": "",
+  "source": "facebook",
+  "author": "",
+  "title": "\u003C3",
+  "thumbnail": "thumbnail url",
+  "duration": 281332,
+  "medias": [
+    {
+      "id": "2385615268505622",
+      "url": "HD VIDEO DOWNLOAD LINK ",
+      "quality": "HD",
+      "type": "video",
+      "extension": "mp4"
+    },
+    {
+      "id": "2385615268505622",
+      "url": "SD VIDEO DOWNLOAD LINK ",
+      "quality": "SD",
+      "type": "video",
+      "extension": "mp4"
+    }
+  ],
+  "type": "multiple",
+  "error": false,
+  "time_end": 1286
+}
