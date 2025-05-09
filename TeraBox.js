374edacc8c3728bedfb1c62653a204d01f490ff6
Narendar761 <145740954+Narendar761@users.noreply.github.com>
2025-05-09 19:53:53 +0530

    Create TeraBox.js

---
+++ TeraBox.js
@@ -0,0 +1,17 @@
+{
+  "✅ Status": "Success",
+  "📜 Extracted Info": [
+    {
+      "📂 Title": "Promo.mkv",
+      "📏 Size": "962.97 KB",
+      "🔽 Direct Download Link": "download link",
+      "🖼️ Thumbnails": {
+        "140x90": "thumbnail 140×90",
+        "360x270": "thumbnail 360×270",
+        "60x60": "thumbnail 60×60",
+        "850x580": "thumbnail 850×580"
+      }
+    }
+  ],
+  "🔗 ShortLink": "https://1024terabox.com/s/120Ir8oL7UBuwuOOifiee6Q"
+}
