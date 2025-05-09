b6185abad6747094c52e53abcde27a3946849634
Narendar761 <145740954+Narendar761@users.noreply.github.com>
2025-05-09 17:51:56 +0530

    Create Instagram.js

---
+++ Instagram.js
@@ -0,0 +1,66 @@
+{
+  "url": "https://www.instagram.com/reel/DJYhLi_BS3h/?utm_source=ig_web_copy_link",
+  "source": "instagram",
+  "title": "asif munir వాడు పెద్ద ఎదవ 🤬 | Venu Kalyan About pahalgam a**ack | In Telugu #armystatus  #pahalgam",
+  "author": "Venu Prasad",
+  "shortcode": "DJYhLi_BS3h",
+  "view_count": 0,
+  "like_count": 122439,
+  "thumbnail": "thumbnail link ",
+  "duration": 58.325,
+  "owner": {
+    "id": "3083444719",
+    "username": "venukalyan01",
+    "is_verified": true,
+    "profile_pic_url": "profile picture download link",
+    "blocked_by_viewer": false,
+    "restricted_by_viewer": null,
+    "followed_by_viewer": false,
+    "full_name": "Venu Prasad",
+    "has_blocked_viewer": false,
+    "is_embeds_disabled": false,
+    "is_private": false,
+    "is_unpublished": false,
+    "requested_by_viewer": false,
+    "pass_tiering_recommendation": true,
+    "edge_owner_to_timeline_media": {
+      "count": 3457
+    },
+    "edge_followed_by": {
+      "count": 341178
+    }
+  },
+  "location": null,
+  "music_attribution_info": {
+    "artist_name": "prshaanthrao",
+    "song_name": "Original audio",
+    "uses_original_audio": true,
+    "should_mute_audio": false,
+    "should_mute_audio_reason": "",
+    "audio_id": "1425281615314677"
+  },
+  "medias": [
+    {
+      "id": "3627795428860571105",
+      "url": " download link mp4",
+      "quality": "640-1137p",
+      "type": "video",
+      "extension": "mp4"
+    },
+    {
+      "id": "616713121375652ad",
+      "type": "audio",
+      "mimeType": "audio/mp4",
+      "codec": "mp4a.40.5",
+      "bandwidth": 70298,
+      "resolution": null,
+      "frameRate": null,
+      "url": " download link audio ",
+      "quality": "audio",
+      "extension": "m4a"
+    }
+  ],
+  "type": "multiple",
+  "error": false,
+  "time_end": 1095
+}
