eca5bb56349c10891af932046c50cf6e2738b9a6
Narendar761 <145740954+Narendar761@users.noreply.github.com>
2025-05-20 17:54:59 +0530

    Create Alll1.js

---
+++ Alll1.js
@@ -0,0 +1,65 @@
+{
+  "url": "https://www.instagram.com/reel/DJl_Q-MJD2f/?utm_source=ig_web_copy_link",
+  "source": "instagram",
+  "title": "✨Trend Set khaali ✌🏻",
+  "author": "padhu padmavathi",
+  "shortcode": "DJl_Q-MJD2f",
+  "view_count": 0,
+  "like_count": 2373176,
+  "thumbnail": "thumbnail link",
+  "duration": 21.1,
+  "owner": {
+    "id": "4941272093",
+    "username": "padhu_padhmavathi6",
+    "is_verified": false,
+    "profile_pic_url": "profile picture link"
+    "restricted_by_viewer": null,
+    "followed_by_viewer": false,
+    "full_name": "padhu padmavathi",
+    "has_blocked_viewer": false,
+    "is_embeds_disabled": false,
+    "is_private": false,
+    "is_unpublished": false,
+    "requested_by_viewer": false,
+    "pass_tiering_recommendation": true,
+    "edge_owner_to_timeline_media": {
+      "count": 1429
+    },
+    "edge_followed_by": {
+      "count": 1248981
+    }
+  },
+  "location": null,
+  "music_attribution_info": {
+    "artist_name": "Dasagriva",
+    "song_name": "MALLASUDU",
+    "uses_original_audio": false,
+    "should_mute_audio": false,
+    "should_mute_audio_reason": "",
+    "audio_id": "594455276911360"
+  },
+  "medias": [
+    {
+      "id": "3631586917761695135",
+      "url": "video download link",
+      "quality": "750-1333p",
+      "type": "video",
+      "extension": "mp4"
+    },
+    {
+      "id": "2129173117561740ad",
+      "type": "audio",
+      "mimeType": "audio/mp4",
+      "codec": "mp4a.40.5",
+      "bandwidth": 60907,
+      "resolution": null,
+      "frameRate": null,
+      "url": "audio link",
+      "quality": "audio",
+      "extension": "m4a"
+    }
+  ], 
+  "type": "multiple",
+  "error": false,
+  "time_end": 1034
+}
